4c70bd80c92d0b0ca55b7d43859b3d6949e373cb
degD <degd@dpc.net>
2026-09-15 10:44:02 +0300

    support Android safe areas

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -3,7 +3,7 @@ import { DataManager } from "./data-manager";
 
 export default function DataPage() {
   return (
-    <main className="min-h-screen bg-white px-4 py-4 text-zinc-950">
+    <main className="min-h-0 flex-1 bg-white px-4 py-4 text-zinc-950">
       <div className="mx-auto flex max-w-4xl flex-col gap-6">
         <Link
           className="w-fit rounded px-1 py-1 font-medium underline focus:outline-2 focus:outline-offset-2 focus:outline-zinc-950"
